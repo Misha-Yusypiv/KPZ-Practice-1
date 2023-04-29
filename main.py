cf1b81020a21ab4c70446bc21a336bb44c5ba06f
Misha-Yusypiv <131203156+Misha-Yusypiv@users.noreply.github.com>
2023-04-29 15:21:23 +0300

    Практична робота №3
    Завдання написати програму на Python, яка буде: 
    Писати слова паліндроми;
    Перевіряти IP-адресу;
    Показувати інформацію про операційну систему;.
    Зберегти програму в GitHub.

--- main.py
+++ main.py
@@ -1,12 +1,35 @@
-def prime_num_generator():
-    num = 2
-    while True:
-        for i in range(2, num):
-            if (num % i) == 0:
-                break
-        else:
-            yield num
-        num += 1
+import re
+import platform
 
-prime = prime_num_generator()
-print([next(prime) for i in range(10)])
+def palindrom(*args):
+    result = []
+    for word in args:
+        if word == word[::-1]:
+            result.append(word)
+    return result
+
+def validate_ip(ip_address):
+    pattern = r'^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}$'
+    if re.match(pattern, ip_address):
+        return True
+    else:
+        return False
+
+def get_os():
+    os_name = platform.system()
+    if os_name == 'Darwin':
+        return 'Mac'
+    elif os_name == 'Windows':
+        return 'Windows'
+    elif os_name == 'Linux':
+        return 'Linux'
+
+print(palindrom("Око", "Дід"))
+ip_address = "192.168.0.1"
+
+print(validate_ip(ip_address))
+print(get_os())
+
+
+
+

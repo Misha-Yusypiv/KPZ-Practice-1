18fa493119edc3c1dc3b8c8bdef0d3492485bf9a
Misha-Yusypiv <131203156+Misha-Yusypiv@users.noreply.github.com>
2023-04-29 12:12:26 +0300

    Практична робота №2
    Завдання написати програму, яка буде генерувати прості числа на Python і зберегти програму в GitHub.

--- main.py
+++ main.py
@@ -1,17 +1,12 @@
-# This is a sample Python script.
+def prime_num_generator():
+    num = 2
+    while True:
+        for i in range(2, num):
+            if (num % i) == 0:
+                break
+        else:
+            yield num
+        num += 1
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
-print("Hello world")
+prime = prime_num_generator()
+print([next(prime) for i in range(10)])

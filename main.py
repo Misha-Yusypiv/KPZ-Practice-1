07d55b7c06d74eef8f9af930230c8fae20267d39
Misha-Yusypiv <131203156+Misha-Yusypiv@users.noreply.github.com>
2023-05-01 19:55:58 +0300

    Практична робота №8
    Парсинг сайту УКД.
    Скрапінг сайту УКД.
    Стягуємо спеціальності університету: УКД.

--- main.py
+++ main.py
@@ -1,112 +1,40 @@
-import sqlite3
-import sys
-from sqlite3 import Connection
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from webdriver_manager.chrome import ChromeDriverManager
 
-global db
-global sql
-db: Connection = sqlite3.connect('accounts')
-sql = db.cursor()
-sql.execute('CREATE TABLE IF NOT EXISTS users (\n'
-            '    login TEXT,\n'
-            '    password TEXT\n'
-            ')')
-db.commit()
-sql.execute("CREATE TABLE IF NOT EXISTS tasks (\n"
-            "    task TEXT\n"
-            ")")
-db.commit()
+driver = webdriver.Chrome(ChromeDriverManager().install())
+URL = "https://ukd.edu.ua/navchalni-plani"
+driver.get(URL)
 
 
-def start_action() -> object:
-    print("Choose you action \n"
-          "        1.Log out\n"
-          "        2.Create new account\n"
-          "        3.Create task\n"
-          "        4.Delete task\n"
-          "        5.Change task\n"
-          "        6.Check the list of tasks\n"
-          "        ")
-    user_input = input('For choosing you action print number of him: ')
-    return user_input
+def print_spec(spec):
+    print("Бакалаврат:")
+    for index, elem in enumerate(spec[0]):
+        print(f"{index}. {elem}")
+    print("Магістратура:")
+    for index, elem in enumerate(spec[1]):
+        print(f"{index}. {elem}")
+    print("Доктор філософії:")
+    for index, elem in enumerate(spec[2]):
+        print(f"{index}. {elem}")
 
 
-def reg():
-    user_login = input("Login: ")
-    user_password = input("Password: ")
-    user_task = ''
-    sql.execute(f"SELECT task FROM tasks WHERE task = '{user_task}'")
-    if sql.fetchone() is not None:
-        pass
-    else:
-        sql.execute(f"INSERT INTO tasks VALUES (?)", (user_task,))
-        db.commit()
-        print("Task field created")
-    sql.execute(f"SELECT login FROM users WHERE login = '{user_login}'")
-    if sql.fetchone() is None:
-        sql.execute(f"INSERT INTO users VALUES (?, ?)", (user_login, user_password))
-        db.commit()
-        print("Registration done!")
-    else:
-        print("Same account already created!")
-    for value in sql.execute("SELECT * FROM users"):
-        print(value)
-    for value in sql.execute("SELECT * FROM tasks"):
-        print(value)
+def get_education_spec(driver):
+    bachelor = list()
+    master = list()
+    doctor_ph = list()
+
+    for ind in range(4, 16):
+        bachelor.append(driver.find_element(By.XPATH,
+                                            f"/html/body/div[1]/main/section/div/div/article/div/div[3]/div/div[2]/div/div/div/table/tbody/tr[{ind}]/td[1]").text)
+    for ind in range(18, 25):
+        master.append(driver.find_element(By.XPATH,
+                                          f"/html/body/div[1]/main/section/div/div/article/div/div[3]/div/div[2]/div/div/div/table/tbody/tr[{ind}]/td[1]").text)
+    doctor_ph.append(driver.find_element(By.XPATH,
+                                         "/html/body/div[1]/main/section/div/div/article/div/div[3]/div/div[2]/div/div/div/table/tbody/tr[27]/td[1]").text)
+
+    print_spec((bachelor, master, doctor_ph))
+    driver.close()
 
 
-def login():
-    user_login = input("Login: ")
-    user_password = input("Password: ")
-
-    sql.execute(f"SELECT login FROM users WHERE login = '{user_login}'")
-    if sql.fetchone() is None:
-        print("Wrong login or password!")
-        user_action = input("Do you wanna create new account? (print 'y' or 'n')")
-        if user_action == 'y':
-            reg()
-        else:
-            False
-    else:
-        print("Login done!")
-        return start_action()
-
-
-def task_action(action):
-    if action == "add":
-        user_task = input("Print the task name: ")
-        sql.execute(f"INSERT INTO tasks VALUES (?)", (user_task,))
-        db.commit()
-    elif action == "delete":
-        user_task = input("Print the task name that you need to delete: ")
-        sql.execute(f"DELETE FROM tasks WHERE task = '{user_task}'")
-        db.commit()
-        print("Task deleted")
-    elif action == "change":
-        user_task = input("Print the task name: ")
-        sql.execute(f"UPDATE tasks SET task = '{user_task}'")
-        db.commit()
-    elif action == "check":
-        for value in sql.execute("SELECT * FROM tasks"):
-            print(value)
-
-
-def user_actions(user_input):
-    if user_input == "1":
-        sys.exit()
-    elif user_input == "2":
-        reg()
-    elif user_input == "3":
-        task_action("add")
-    elif user_input == "4":
-        task_action("delete")
-    elif user_input == "5":
-        task_action("change")
-    elif user_input == "6":
-        task_action("check")
-
-
-def main():
-    user_actions(login())
-
-
-main()
+get_education_spec(driver)
